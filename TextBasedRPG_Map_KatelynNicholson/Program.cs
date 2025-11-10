12fbf02cc448018d7dd15a2011ca991203f635aa
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-11-10 16:40:58 -0400

    push

--- TextBasedRPG_Map_KatelynNicholson/Program.cs
+++ TextBasedRPG_Map_KatelynNicholson/Program.cs
@@ -31,32 +31,30 @@ namespace TextBaseRPGMap_KatelynNicholson_2025_10_27
         static void Main()
         {
             DisplayMap();
-            //DisplayMap(2);
-            //DisplayMap(3);
+            DisplayMap(2);
+            DisplayMap(3);
         }
 
         static void DisplayMap() //scale 1
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("X");
+            Console.Write("X"); //Top Left Corner
             for(int ex = 0; ex < map.GetLength(1); ex++)
             {
-                Console.Write("-");
-
+                Console.Write("-"); //Top Border
 
             }
-            Console.WriteLine("X");
+            Console.WriteLine("X"); //Top right corner
 
             for (int y = 0; y < map.GetLength(0); y++)
             {
                 //border
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("X");
+                Console.Write("|"); //Left Border
 
                 for (int x = 0; x < map.GetLength(1); x++)
                 {
                     char tile = map[y, x];
-
                     switch (tile)
                     {
                         case '▒':
@@ -80,34 +78,43 @@ namespace TextBaseRPGMap_KatelynNicholson_2025_10_27
                 }
                 //makes new line for y && border
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("X");
+                Console.WriteLine("|"); //right border
 
             }
             //border
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("X");
+            Console.Write("X");//Bottom Left Corner
             for (int ex = 0; ex < map.GetLength(1); ex++) //* scale 
             {
-                Console.Write("-");
-
-
+                Console.Write("-"); //Left Border
             }
-            Console.WriteLine("X");
+            Console.WriteLine("X"); //Bottom Right Border
         }
 
         static void DisplayMap(int scale) // scale of 2 and scale of 3 
         {
-            //conditionals for boarder
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("X"); //Top Left Corner
+            for (int ex = 0; ex < map.GetLength(1) * scale; ex++)
+            {
+                Console.Write("-"); //Top Border
+
+            }
+            Console.WriteLine("X"); //Top right corner
+
             for (int y = 0; y < map.GetLength(0); y++)
             {
                 for (int alerow = 0; alerow < scale; alerow++) 
-                { 
+                {
+                    //border
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("|"); //Left Border
+
                     for (int x = 0; x < map.GetLength(1); x++)
                     {
                         for(int alecol = 0; alecol < scale; alecol++)
                         {
                             char tile = map[y, x];
-
                             switch (tile)
                             {
                                 case '▒':
@@ -125,12 +132,25 @@ namespace TextBaseRPGMap_KatelynNicholson_2025_10_27
                                     break;
                             }
                             Console.Write(tile);
+
+                            //reset background color
                             Console.BackgroundColor = ConsoleColor.Black;
                         }
                     }
-                    Console.WriteLine();
+                    //makes new line for y && border
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("|"); //right border
                 }
             }
+
+            //border
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("X");//Bottom Left Corner
+            for (int ex = 0; ex < map.GetLength(1) * scale; ex++) //* scale 
+            {
+                Console.Write("-"); //Left Border
+            }
+            Console.WriteLine("X"); //Bottom Right Corner
         }
     }
 }
